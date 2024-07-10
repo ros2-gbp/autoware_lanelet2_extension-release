a3b3f7c9e055a39907f7ba3fc0ea5e8c43f959ce
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-07-10 16:12:37 +0900

    Imported upstream version '0.5.0' of 'upstream'

--- autoware_lanelet2_extension/include/autoware_lanelet2_extension/regulatory_elements/no_parking_area.hpp
+++ autoware_lanelet2_extension/include/autoware_lanelet2_extension/regulatory_elements/no_parking_area.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef LANELET2_EXTENSION__REGULATORY_ELEMENTS__NO_PARKING_AREA_HPP_
-#define LANELET2_EXTENSION__REGULATORY_ELEMENTS__NO_PARKING_AREA_HPP_
+#ifndef AUTOWARE_LANELET2_EXTENSION__REGULATORY_ELEMENTS__NO_PARKING_AREA_HPP_
+#define AUTOWARE_LANELET2_EXTENSION__REGULATORY_ELEMENTS__NO_PARKING_AREA_HPP_
 
 // NOLINTBEGIN(readability-identifier-naming)
 
@@ -24,6 +24,9 @@
 #include <memory>
 
 namespace lanelet::autoware
+{
+
+inline namespace format_v1
 {
 class NoParkingArea : public lanelet::RegulatoryElement
 {
@@ -64,9 +67,10 @@ private:
   friend class RegisterRegulatoryElement<NoParkingArea>;
   explicit NoParkingArea(const lanelet::RegulatoryElementDataPtr & data);
 };
+}  // namespace format_v1
 
 }  // namespace lanelet::autoware
 
 // NOLINTEND(readability-identifier-naming)
 
-#endif  // LANELET2_EXTENSION__REGULATORY_ELEMENTS__NO_PARKING_AREA_HPP_
+#endif  // AUTOWARE_LANELET2_EXTENSION__REGULATORY_ELEMENTS__NO_PARKING_AREA_HPP_
